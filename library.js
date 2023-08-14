cf7108e5280dd58a797c38b696dc3859927b2d7a
Manishshrivastva60 <manish.shrivastva60@gmail.com>
2023-08-14 15:35:17 +0530

    commit

---
+++ library.js
@@ -0,0 +1,14 @@
+// Module ES6: 
+// A module organizes a related set of javascript <code class=""></code>
+// A module can contain variale classes and functions.
+// A module is m=nothing more then a chunk of javascript code.
+
+
+
+
+export let message="Ducat India"
+
+export function add(name)
+{
+    console.log(`$(name)`)
+}
